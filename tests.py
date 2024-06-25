840aa91604a896f5011da97f3e0f91a8cd131049
Natalia Peguero <pegueronatalia@gmail.com>
2024-06-25 18:51:45 -0400

    fixing PEP8 for tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 import unittest
-from main import getUserInput, additionalQuestion, userFeedback, sendApiRequest, process_response, modify_database
+from main import getUserInput, additionalQuestion
+from main import userFeedback, sendApiRequest, process_response
+from main import modify_database
 from unittest.mock import patch, MagicMock
 import pandas as pd
 
@@ -10,18 +12,20 @@ class TestMediaRecommendations(unittest.TestCase):
     def test_getUserInput(self):
         with patch('builtins.input', side_effect=['Batman', 'S']):
             choices = getUserInput()
-            self.assertEqual(choices,['Batman'])
+            self.assertEqual(choices, ['Batman'])
 
     def test_getError(self):
-        with patch('builtins.input', side_effect=['S','Batman','S']):
+        with patch('builtins.input', side_effect=['S', 'Batman', 'S']):
             choices = getUserInput()
-            self.assertEqual(choices,['Batman'])
+            self.assertEqual(choices, ['Batman'])
 
     def test_getSixInputs(self):
-        with patch('builtins.input', side_effect=['Superman','Batman','Spiderman', 'Hulk', 'Wonder Woman', 'Green Lantern']):
+        with patch('builtins.input', side_effect=[
+              'Superman', 'Batman', 'Spiderman', 'Hulk',
+              'Wonder Woman', 'Green Lantern']):
             choices = getUserInput()
-            self.assertEqual(choices,['Superman','Batman','Spiderman', 'Hulk', 'Wonder Woman'])
-
+            self.assertEqual(choices, ['Superman', 'Batman',
+                                       'Spiderman', 'Hulk', 'Wonder Woman'])
 
     # additionalQuestion
     @patch('builtins.input', side_effect=['Sci-Fi'])
@@ -33,7 +37,6 @@ class TestMediaRecommendations(unittest.TestCase):
     def test_additionalQuestion(self, mock_input):
         preferences = additionalQuestion()
         self.assertEqual(preferences, '')
-
 
     # userFeedback
     @patch('builtins.input', side_effect=['Y'])
@@ -51,13 +54,12 @@ class TestMediaRecommendations(unittest.TestCase):
         feedback = userFeedback()
         self.assertEqual(feedback, '')
 
-    
-    # process_response   
+    # process_response
     def test_process_response(self):
         formatted_data = {'recommendations': [{
-             'title': 'Movie1', 
-             'genre': 'Action', 
-             'rating': 8.5, 
+             'title': 'Movie1',
+             'genre': 'Action',
+             'rating': 8.5,
              'release date': '2020-01-01'
              }
           ]
@@ -66,28 +68,20 @@ class TestMediaRecommendations(unittest.TestCase):
         self.assertEqual(len(recommendations), 1)
         self.assertEqual(recommendations[0]['title'], 'Movie1')
 
-
     # modify_database
     @patch('pandas.DataFrame.to_sql')
     def test_modify_database(self, MockDataFrame):
         recommendations = [{
-            'title': 'Movie1', 
-            'genre': 'Action', 'rating': 8.5, 
+            'title': 'Movie1',
+            'genre': 'Action', 'rating': 8.5,
             'releaseDate': '2020-01-01'
             }
         ]
         modify_database(recommendations)
         MockDataFrame.assert_called_once()
 
-
     # API
 
-
-
-    
-    
-
-       
 
 if __name__ == '__main__':
     unittest.main()
